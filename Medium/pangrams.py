76d6fec37c3229970244292a42d154efea4e30e5
Alex.Eggers <alex.eggers@xailabs.de>
2016-09-26 10:51:49 +0200

    Small change to streamline code
    Uses set now instead of list, not allowing for repeat characters

--- Medium/pangrams.py
+++ Medium/pangrams.py
@@ -1,18 +1,13 @@
 import string
+from sys import argv
 
 file_name = "pangrams.txt"
 
 with open(file_name, "r") as file:
-    cases = [x for x in file.read().splitlines() if x]
+    cases = [set(x.lower()) for x in file.read().splitlines() if x]
 
-alphabet = list(string.ascii_lowercase)
+alphabet = set(string.ascii_lowercase)
 
 for case in cases:
-    present = []
-    for letter in alphabet:
-        if letter in case:
-            present.append(letter)
-    if present:
-        print("".join(present))
-    else:
-        print("NULL")
+    missing = alphabet - case
+    print("".join(missing or "NULL")
